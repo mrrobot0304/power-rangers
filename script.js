80cdb039f8890c5177fcd7bce008ba5acc37e1ef
mrrobot0304 <136024760+mrrobot0304@users.noreply.github.com>
2024-10-23 00:48:25 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const myFunction = () => {
+    document.getElementById("first").style.display ='block';
+    document.getElementById("second").style.display ='none';
+    document.getElementById("third").style.display ='none';
+    document.getElementById("four").style.display ='none';
+    document.getElementById("five").style.display ='none';
+}
+
+const myFunction2 = () => {
+    document.getElementById("second").style.display ='block';
+    document.getElementById("first").style.display ='none';
+    document.getElementById("third").style.display ='none';
+    document.getElementById("four").style.display ='none';
+    document.getElementById("five").style.display ='none';
+}
+
+const myFunction3 = () => {
+    document.getElementById("third").style.display ='block';
+    document.getElementById("first").style.display ='none';
+    document.getElementById("second").style.display ='none';
+    document.getElementById("four").style.display ='none';
+    document.getElementById("five").style.display ='none';
+}
+
+const myFunction4 = () => {
+    document.getElementById("four").style.display ='block';
+    document.getElementById("third").style.display ='none';
+    document.getElementById("first").style.display ='none';
+    document.getElementById("second").style.display ='none';
+    document.getElementById("five").style.display ='none';
+}
+
+const myFunction5 = () => {
+    document.getElementById("five").style.display ='block';
+    document.getElementById("four").style.display ='none';
+    document.getElementById("third").style.display ='none';
+    document.getElementById("first").style.display ='none';
+    document.getElementById("second").style.display ='none';
+}
